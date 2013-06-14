f20a96ca060b39a5baa3c8f6b55e549e823bdc77
Jonathan Paprocki <Paprocjo@gmail.com>
2013-06-14 07:14:56 -0500

    714 614 Added G1S2 LVL

--- Unity Project/Assets/Scripts/GUI/EndStageGUI.cs
+++ Unity Project/Assets/Scripts/GUI/EndStageGUI.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class EndStageGUI : MonoBehaviour 
 {
     bool isOn = false;
+    public int loadLevel;
 
     public void Activate()
     {
@@ -18,6 +19,7 @@ public class EndStageGUI : MonoBehaviour
             GUI.Label(new Rect((Screen.width/2) - 50, Screen.height*.35f, 100, 20), "Level Complete!");
             if (GUI.Button(new Rect((Screen.width/4)-100, (Screen.height * .45f), 200, 40), "Next Level"))
             {
+                Application.LoadLevel(loadLevel);
             }
             if (GUI.Button(new Rect(((Screen.width / 4)*3) - 100, (Screen.height * .45f), 200, 40), "Main Menu"))
             {
